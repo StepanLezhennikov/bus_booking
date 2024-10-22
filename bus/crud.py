286035bd16f459b5540d8b722ef5c63e1dc1f975
Stepan Lezhennikov <stepanlezennikov@gmail.com>
2024-10-22 11:05:17 +0300

    CRUD for buses

--- bus/crud.py
+++ bus/crud.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from .models import Bus
 from .schemas import BusCreate
@@ -9,3 +10,27 @@ async def create_bus(db: AsyncSession, bus: BusCreate):
     await db.commit()
     await db.refresh(new_bus)
     return new_bus
+
+
+async def get_buses(db: AsyncSession, skip: int = 0, limit: int = 10):
+    query = select(Bus).offset(skip).limit(limit)
+    result = await db.execute(query)
+    buses = result.scalars().all()
+    return buses
+
+
+async def get_bus(bus_id: int, db: AsyncSession):
+    query = select(Bus).filter(Bus.id == bus_id)
+    bus = await db.execute(query)
+    return bus.scalars().first()
+
+
+async def delete_bus(bus_id: int, db: AsyncSession):
+    query = select(Bus).filter(Bus.id == bus_id)
+    result = await db.execute(query)
+    bus = result.scalar_one_or_none()
+    if bus:
+        await db.delete(bus)
+        await db.commit()
+        return bus
+    return None
--- bus/routes.py
+++ bus/routes.py
@@ -1,15 +1,36 @@
-from fastapi import APIRouter, Depends
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_db
 from .schemas import Bus, BusCreate
-from .crud import create_bus as create_new_bus
+from .crud import create_bus as create_new_bus, get_buses as get_all_buses, get_bus as get_one_bus, delete_bus as delete_one_bus
 
-router = APIRouter(tags=["Работа с автобусами"])
+router = APIRouter(prefix="/buses", tags=["Работа с автобусами"])
 
 
-@router.post('/buses/')
+@router.post('/', description='Создание автобуса')
 async def create_bus(bus: BusCreate, db: AsyncSession = Depends(get_db)) -> Bus:
     new_bus = await create_new_bus(db, bus)
     return Bus.from_orm(new_bus)
 
 
+@router.get('/', description='Получение всех автобусов')
+async def get_buses(skip: int = 0, limit: int = 10, db: AsyncSession = Depends(get_db)) -> List[Bus]:
+    return await get_all_buses(db, skip=skip, limit=limit)
+
+
+@router.get('/{bus_id}', description='Получение одного автобуса')
+async def get_bus(bus_id: int, db: AsyncSession = Depends(get_db)) -> Bus:
+    bus = await get_one_bus(bus_id, db)
+    if not bus:
+        raise HTTPException(status_code=404, detail="Автобус не найден")
+    return bus
+
+
+@router.delete('/{bus_id}',description="Удаление автобуса")
+async def delete_bus(bus_id: int, db: AsyncSession = Depends(get_db)) -> Bus:
+    bus = await  delete_one_bus(bus_id, db)
+    if not bus:
+        raise HTTPException(status_code=404, detail="Автобус не найден")
+    return bus
